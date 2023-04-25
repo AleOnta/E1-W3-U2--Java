d76ec67e5135aa4ff74e746eaf5b56749378af3b
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-04-25 16:31:28 +0200

    adds entities rest_api controllers

---
+++ src/main/java/com/api_rest/E1W3U2JavaApplication.java
@@ -0,0 +1,13 @@
+package com.api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E1W3U2JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E1W3U2JavaApplication.class, args);
+	}
+
+}
