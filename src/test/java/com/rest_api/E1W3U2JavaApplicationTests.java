6393d61c9f7ef6dbd389780a8cb431d643c652ba
Alessandro <114835798+AleOnta@users.noreply.github.com>
2023-04-25 16:28:12 +0200

    initialized project

---
+++ src/test/java/com/rest_api/E1W3U2JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E1W3U2JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
